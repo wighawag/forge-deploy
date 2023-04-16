74782190c84478fbaed7acb7c0f5473011ea8fcc
wighawag <wighawag@gmail.com>
2023-04-16 07:20:39 +0100

    basic setup

--- main.rs
+++ main.rs
@@ -1,10 +1,10 @@
-use std::{fs, collections::HashMap};
+// use std::{fs, collections::HashMap};
 
-use handlebars::Handlebars;
-use serde::{Deserialize, Serialize};
-use serde_json::{Value, from_str};
-use serde_json::Value::Object;
-use regex::Regex;
+// use handlebars::Handlebars;
+// use serde::{Deserialize, Serialize};
+// use serde_json::{Value, from_str};
+// use serde_json::Value::Object;
+// use regex::Regex;
 use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
@@ -17,23 +17,34 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    /// Adds files to myapp
-    Add { name: Option<String> },
+    /// Sync last broadcast with deployments
+    Sync {name: Option<String> },
+    // Generate Deployer Helper with Artifacts found
+    Artifacts
 }
 
 fn main() {
     let cli = Cli::parse();
 
-    // You can check for the existence of subcommands, and if found use their
-    // matches just as you would the top level cmd
     match &cli.command {
         Some(command) => {
-            match(command) {
-                Commands::Add { name } => {
-                    println!("'myapp add' was used, name is: {name:?}")
-                }
+            match command {
+                Commands::Sync { name } => sync(name),
+                Commands::Artifacts => artifacts()
             }
         }
-        None => println!("'myapp'")
+        None => top()
     }
+}
+
+fn sync(name: &Option<String>) {
+    println!("'forge-deploy sync' was used, name is: {name:?}")
+}
+
+fn artifacts() {
+    println!("generating deployer...")
+}
+
+fn top() {
+    println!("'myapp'")
 }
