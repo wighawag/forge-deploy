efe095015ba761fca1f92a002ddd6c91dbeb4e89
wighawag <wighawag@gmail.com>
2023-04-17 21:01:39 +0100

    add another contract examples

---
+++ examples/basic/src/AnotherTokens.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: AGPL-1.0
+pragma solidity ^0.8.13;
+
+contract AnotherTokens {
+    constructor(address to, uint256 amount) {}
+
+    string public constant symbol = "TOKENS";
+}
