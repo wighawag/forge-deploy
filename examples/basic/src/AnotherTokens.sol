1ab926877d312923828cdb99c5047be7f98b71af
wighawag <wighawag@gmail.com>
2023-04-18 12:04:02 +0100

    caretaker with src-artifacts

--- examples/basic/src/AnotherTokens.sol
+++ examples/basic/src/AnotherTokens.sol
@@ -4,11 +4,18 @@ pragma solidity ^0.8.13;
 contract AnotherTokens {
     constructor(address to, uint256 amount) {}
 
-    string public constant symbol = "ANOTHER_TOKENS";
+    string public constant symbol = "ANOTHER_TOKENS_4";
 }
 
 
-contract AnotherTokens2 {
+contract AnotherTokens3 {
+    constructor(address to, uint256 amount) {}
+
+    string public constant symbol = "ANOTHER_TOKENS_2";
+}
+
+
+contract AnotherTokens4 {
     constructor(address to, uint256 amount) {}
 
     string public constant symbol = "ANOTHER_TOKENS_2";
