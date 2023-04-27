9ea4ee0f0fde85e97fc9c459de978ced3031e8a5
wighawag <wighawag@gmail.com>
2023-04-27 15:01:22 +0100

    v0.0.21

--- build-npm.js
+++ build-npm.js
@@ -8,20 +8,36 @@ execFileSync("cargo", ["install", "cargo-release"], {stdio});
 execFileSync("cargo", ["install", "rust-to-npm"], {stdio});
 execFileSync("npm", ["i", "-g", "rust-to-npm"], {stdio});
 execFileSync("rust-to-npm", ["build"], {stdio});
+
+// ------------------------------------------------------------------------------------------------
+// FIX package.json // TODO use own template
+// ------------------------------------------------------------------------------------------------
 const pkg = JSON.parse(fs.readFileSync("package.json", 'utf-8'));
 pkg.files.push("contracts");
 delete pkg.main;
 pkg.bin = {
-    // "forge-deploy-js": "start.js", // this was used to tets the updated start.js, no need anymore, keep for now if issues arise
-    "forge-deploy": "bin/forge-deploy"
+    "forge-deploy": "start.js", // This seems needed as npm will install the bin script first and fails to fetch bin/forge-deploy then
+    "forge-deploy-binary": "bin/forge-deploy"
 }
+delete pkg.scripts.uninstall;
+pkg.scripts.preinstall = pkg.scripts.postinstall;
+delete pkg.scripts.postinstall;
 fs.writeFileSync("package.json", JSON.stringify(pkg, null, 2));
+// ------------------------------------------------------------------------------------------------
 
+// ------------------------------------------------------------------------------------------------
+// FIX pre-install.js to install locally
+// ------------------------------------------------------------------------------------------------
 const install_process = fs.readFileSync("pre-install.js", "utf-8");
 fs.writeFileSync("pre-install.js", install_process.replace("cargo install forge-deploy", "cargo install --root . forge-deploy"))
+// ------------------------------------------------------------------------------------------------
 
+// ------------------------------------------------------------------------------------------------
+// FIX start.js to use the local binary
+// ------------------------------------------------------------------------------------------------
 const start_process = fs.readFileSync("start.js", "utf-8");
 fs.writeFileSync("start.js", start_process.replace(`"forge-deploy"`, `\`\${__dirname}/bin/forge-deploy \${process.argv.slice(2).join(' ')}\``));
+// ------------------------------------------------------------------------------------------------
 
 if (args[0] === 'publish') {
     execFileSync("cargo", ["release", "--execute"], {stdio});
