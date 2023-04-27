3306e53d306cf024d1f1d149205e5331d94ba708
wighawag <wighawag@gmail.com>
2023-04-27 11:34:27 +0100

    v0.0.18

--- build-npm.js
+++ build-npm.js
@@ -10,7 +10,14 @@ execFileSync("npm", ["i", "-g", "rust-to-npm"], {stdio});
 execFileSync("rust-to-npm", ["build"], {stdio});
 const pkg = JSON.parse(fs.readFileSync("package.json", 'utf-8'));
 pkg.files.push("contracts");
+delete pkg.main;
+pkg.bin = {
+    "forge-deploy": "start.js"
+}
 fs.writeFileSync("package.json", JSON.stringify(pkg, null, 2));
+
+const install_process = fs.readFileSync("pre-install.js", "utf-8");
+fs.writeFileSync("pre-install.js", install_process.replace("cargo install forge-deploy", "cargo install --root bin forge-deploy"))
 
 if (args[0] === 'publish') {
     execFileSync("cargo", ["release", "--execute"], {stdio});
